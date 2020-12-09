2983e6f7afb9500e29ed0afe5519c111066dc5b8
Falak <falak781@hotmail.com>
2020-12-09 10:00:17 +0000

    date of birth - additional detail to customer

---
+++ server/DateOfBirth.java
@@ -0,0 +1,15 @@
+package server;
+
+public class DateOfBirth {
+    private String DateOfBirth;
+    public DateOfBirth() {
+    }
+
+    public String getDateOfBirth() {
+        return DateOfBirth;
+    }
+
+    public void setDateOfBirth(String DateOfBirth) {
+        this.DateOfBirth = DateOfBirth;
+    }
+}
